cf14732e38a59b27daeac80b05e92bf4aa75aab5
Nikita <itstudiolinux@gmail.com>
2024-11-15 12:43:12 +0300

    redact text

--- firstproject/my_app/views.py
+++ firstproject/my_app/views.py
@@ -7,13 +7,14 @@ from django.urls import reverse
 # Create your views here.
 
 def index_views(request: HttpRequest) -> HttpResponse:
-    return HttpResponse(f"<h1><a href={reverse('time')}>Узнать текущее время</a></h1>"
+    return HttpResponse(f"<h1>Выбирите Раздел</h1>"
+                        f"<a href={reverse('time')}>Узнать текущее время</a>"
                         f"<br>"
-                        f"<h1><a href={reverse('workdir')}>Содержимое рабочей директории</a></h1>")
+                        f"<a href={reverse('workdir')}>Содержимое рабочей директории</a>")
 
 def time_views(request: HttpRequest) -> HttpResponse:
     time_now = datetime.now().time()
-    return HttpResponse(f"<h1>Текущее время: {time_now}</h2><br><h1><a href={reverse('index')}>Вернутся на главную страницу</a></h1>")
+    return HttpResponse(f"<h1>Текущее время: {time_now}</h2><br><a href={reverse('index')}>Вернутся на главную страницу</a>")
 
 def workdir_views(request: HttpRequest) -> HttpResponse:
     out_command = os.popen('pwd').read()
@@ -23,4 +24,4 @@ def workdir_views(request: HttpRequest) -> HttpResponse:
     return HttpResponse(f"<h1>Рабочий католог: {out_command}</h1>"
                         f"<h1>Содержимое каталога: {dir_new}</h1>"
                         f"<br>" 
-                        f"<h1><a href={reverse('index')}>Вернутся на главную страницу</a></h1>")
+                        f"<a href={reverse('index')}>Вернутся на главную страницу</a>")
